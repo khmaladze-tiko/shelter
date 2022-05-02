52ef6a73635a0231dfdd163be3e0f04e737b9cda
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-05-02 04:02:05 +0400

    fix: slider in script.js

---
+++ pages/main/script.js
@@ -0,0 +1,31 @@
+function OurFriendsButton(e){
+    window.location.hash="ourFriends";
+}
+function buttonToOurFreinds(e){
+    window.location.hash="../pets/index.html"
+}
+const cardWrap = document.querySelector(".cards-box");
+const card = document.getElementsByClassName("card");
+
+let counter = 0;
+
+const leftArrow = document.querySelector(".leftArrow");
+const rightArrow = document.querySelector(".rightArrow");
+
+leftArrow.addEventListener("click", ()=>{
+    if(counter == 0) {
+        counter=card.length-2
+    }if(counter !== 0){
+        counter--;
+    }
+    cardWrap.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+});
+
+rightArrow.addEventListener('click',()=>{
+    if(counter == card.length-3){
+      counter=-1;
+  }if(counter <= card.length-4){
+      counter++
+  }
+  cardWrap.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+})
